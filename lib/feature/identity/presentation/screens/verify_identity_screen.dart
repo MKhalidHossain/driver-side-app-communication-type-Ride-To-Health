c15181ae5331c710039b160ac736550d68b7f0de
MKhalidHossain <compilefiller@gmail.com>
2025-07-29 16:23:20 +0600

    id verification done jemon temon

--- lib/feature/identity/presentation/screens/verify_identity_screen.dart
+++ lib/feature/identity/presentation/screens/verify_identity_screen.dart
@@ -77,6 +77,8 @@ class VerifyIdentityScreen extends StatelessWidget {
                 ),
               ),
             ),
+
+            // Selfie
             GestureDetector(
               onTap: () {},
 
