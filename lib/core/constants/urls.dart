6d39b447ff3693a4d6ac74f762c1dac645fd3949
MKhalidHossain <compilefiller@gmail.com>
2026-01-18 14:24:09 +0600

    fix: Update AndroidManifest.xml to remove sensitive permissions and adjust URLs in constants

--- lib/core/constants/urls.dart
+++ lib/core/constants/urls.dart
@@ -1,14 +1,12 @@
 class Urls {
   // Base URL
   // static const String baseUrl = 'http://localhost:5001/api';
-  // static const String baseUrl = 'http://10.0.2.2:5001/api';
-  static const String baseUrl = 'https://api.rideztransportation.com/api';
-  // static const String baseUrl = 'http://10.10.5.94:5001/api';
+  // static const String baseUrl = 'https://api.rideztransportation.com/api';
+  static const String baseUrl = 'http://10.10.5.85:5000/api';
   // static const String baseUrl = 'https://ridetohealth-backend.onrender.com/api';
 
-  // static const String socketUrl = 'http://10.10.5.94:5001';
-  // static const String socketUrl = 'http://10.0.2.2:5001';
-  static const String socketUrl = 'https://api.rideztransportation.com';
+  static const String socketUrl = 'http://10.10.5.85:5000';
+  // static const String socketUrl = 'https://api.rideztransportation.com';
   // static const String socketUrl = 'https://ridetohealth-backend.onrender.com';
   // ------------------------ Authentication ------------------------
 
